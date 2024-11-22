d615d121ea9e50f2a6d21edacfb11654c1fe39bc
Aayushman Sharma <075bme004aayushman@gmail.com>
2024-11-22 07:11:16 +0545

    feat: horizontal scroll animation using framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,47 @@
+"use client";
+
+import { useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+
+const IMAGES = [
+  { id: 1, image: "/images/media_1.png" },
+  { id: 2, image: "/images/media_2.png" },
+  { id: 3, image: "/images/media_3.jpg" },
+  { id: 4, image: "/images/media_4.png" },
+  { id: 5, image: "/images/media_5.png" },
+  { id: 6, image: "/images/media_6.jpg" },
+  { id: 7, image: "/images/media_7.png" },
+  { id: 8, image: "/images/media_8.png" },
+  { id: 9, image: "/images/media_9.jpg" },
+  { id: 10, image: "/images/media_10.png" },
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({ target: sectionRef });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-71.43%"]);
+  return (
+    <main>
+      <header>Header</header>
+      <div className="main-wrapper" ref={sectionRef}>
+        <div className="main">
+          <motion.div style={{ x }}>
+            <div className="image-container">
+              {IMAGES.map((item, index) => {
+                return (
+                  <figure key={index}>
+                    <Image src={item.image} alt="" fill />
+                  </figure>
+                );
+              })}
+            </div>
+          </motion.div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <footer>Footer</footer>
+    </main>
   );
 }
